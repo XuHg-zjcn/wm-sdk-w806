af8f288ab44d1195d3d05593b7bfa9af47d5b51e
徐瑞骏 <1687701765@qq.com>
2021-11-20 20:55:36 +0800

    驱动头文件添加extern "C"

--- include/driver/wm_spi.h
+++ include/driver/wm_spi.h
@@ -146,6 +146,10 @@ typedef struct __SPI_HandleTypeDef
 
 #define __HAL_SPI_CELAR_FLAG(__HANDLE__, FLAG)    SET_BIT((__HANDLE__)->Instance->INT_SRC, FLAG)
 
+
+#ifdef __cplusplus
+extern "C"{
+#endif
 HAL_StatusTypeDef HAL_SPI_Init(SPI_HandleTypeDef *hspi);
 HAL_StatusTypeDef HAL_SPI_DeInit(SPI_HandleTypeDef *hspi);
 void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi);
@@ -155,7 +159,9 @@ HAL_StatusTypeDef HAL_SPI_Transmit(SPI_HandleTypeDef *hspi, uint8_t *pData, uint
 HAL_StatusTypeDef HAL_SPI_Receive(SPI_HandleTypeDef *hspi, uint8_t *pData, uint32_t Size, uint32_t Timeout);
 HAL_StatusTypeDef HAL_SPI_TransmitReceive(SPI_HandleTypeDef *hspi, uint8_t *pTxData, uint8_t *pRxData, uint32_t Size,
                                           uint32_t Timeout);
-
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif
