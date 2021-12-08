00aa19d01bf90fa92ff87eebd2e04327bb89778b
徐瑞骏 <1687701765@qq.com>
2021-12-08 18:30:59 +0800

    移动到demo/fatfs文件夹

--- demo/fatfs/main.c
+++ demo/fatfs/main.c
@@ -1,18 +1,3 @@
-/******************************************************************************
-** 
- * \file        main.c
- * \author      IOsetting | iosetting@outlook.com
- * \date        
- * \brief       Demo code of PWM in independent mode
- * \note        This will drive 3 on-board LEDs to show fade effect
- * \version     v0.1
- * \ingroup     demo
- * \remarks     test-board: HLK-W806-KIT-V1.0
- *              PWM Frequency = 40MHz / Prescaler / (Period + 1)；
-                Duty Cycle(Edge Aligned)   = (Pulse + 1) / (Period + 1)
-                Duty Cycle(Center Aligned) = (2 * Pulse + 1) / (2 * (Period + 1))
- *
-******************************************************************************/
 
 #include <stdio.h>
 #include "wm_hal.h"
